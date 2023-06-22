e974b743f04078ea6adb82c18857feb9e8a359e3
NikolayP <nikipanovski@gmail.com>
2023-06-22 23:33:00 +0300

    Include latest TestRunners in main.cpp

--- TTT_EmbedInCPP/LuaTestMethods/OverwritePlayerVar_LuaToCPP.cpp
+++ TTT_EmbedInCPP/LuaTestMethods/OverwritePlayerVar_LuaToCPP.cpp
@@ -1,9 +1,6 @@
 #include "OverwritePlayerVar_LuaToCPP.h"
-#include "../Entities/Player.h"
 
 void OverwritePlayerVar_LuaToCPP::Run(lua_State* lua) {
-	Player player;
-
 	lua_getglobal(lua, "lastLevelSpecialSpawn");
 
 	if (lua_isnumber(lua, -1)) {
--- TTT_EmbedInCPP/LuaTestMethods/OverwritePlayerVar_LuaToCPP.h
+++ TTT_EmbedInCPP/LuaTestMethods/OverwritePlayerVar_LuaToCPP.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "LuaTestMethod.h"
+#include "../Entities/Player.h"
 
 class OverwritePlayerVar_LuaToCPP : public LuaTestMethod
 {
+	private:
+		Player player;
+
 	public:
 		void Run(lua_State* lua) override;
 };
